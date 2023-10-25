7385390961c2b0b570a768cc37903618f64fdeaf
Akinyemi Oluwabukunmi <braingrams40@gmail.com>
2023-10-24 21:16:19 -0700

    Fixed dara website metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,16 +31,16 @@ export const metadata: Metadata = {
   },
   themeColor: '#FFFFFF',
   openGraph: {
-    url: 'https://nextarter-chakra.sznm.dev',
+    url: 'https://usedara.com',
     title: 'Dara website',
-    description: 'Next.js + chakra-ui + TypeScript template',
+    description: 'Services delivered smart',
     images: {
-      url: 'https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
-      alt: 'nextarter-chakra.sznm.dev og-image',
+      url: '',
+      alt: 'Dara website',
     },
   },
   twitter: {
-    creator: '@sozonome',
+    creator: '@thatguycalledbrain',
     card: 'summary_large_image',
   },
 };
